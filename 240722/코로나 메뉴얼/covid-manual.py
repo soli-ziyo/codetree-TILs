2849028755d7f0b1121eb7bafbec0c6a9a12ac83
minsol <163154019+soli-ziyo@users.noreply.github.com>
2024-07-22 21:23:39 +0900

    Title: 코로나 메뉴얼, Time: 138ms, Memory: 24MB, Status: Passed - CodeTree

--- 240722/코로나 메뉴얼/covid-manual.py
+++ 240722/코로나 메뉴얼/covid-manual.py
@@ -1,27 +1,21 @@
-cold1, temp1 = input().split()
-cold2, temp2= input().split()
-cold3, temp3 = input().split()
+# 변수 선언 및 입력
+inp = input().split()
+c1, t1 = inp[0], int(inp[1])
+inp = input().split()
+c2, t2 = inp[0], int(inp[1])
+inp = input().split()
+c3, t3 = inp[0], int(inp[1])
 
-temp1, temp2, temp3 = int(temp1), int(temp2), int(temp3)
-
-if (cold1 == "Y" and temp1 >=37):
-    if (cold2 == "Y" and temp2>=37):
-        print("E")
-    elif (cold3 == "Y" and temp3>=37):
+# A가 2명 이상인지 판단하기
+if c1 == 'Y' and t1 >= 37:
+    # 첫 번째 사람이 A라면, 남은 두 사람 중 한 사람이라도 A면 됩니다.
+    if (c2 == 'Y' and t2 >= 37) or (c3 == 'Y' and t3 >= 37):
         print("E")
     else:
         print("N")
-elif (cold2 =="Y" and temp2 >=37):
-    if (cold1 == "Y" and temp1>=37):
-        print("E")
-    elif (cold3 == "Y" and temp3>=37):
-        print("E")
-    else:
-        print("N")
-elif (cold3 =="Y" and temp3 >=37):
-    if (cold1 == "Y" and temp1>=37):
-        print("E")
-    elif (cold2 == "Y" and temp2>=37):
+else:
+    # 첫 번째 사람이 A가 아니라면, 남은 두 사람 모두 A여야만 합니다.
+    if (c2 == 'Y' and t2 >= 37) and (c3 == 'Y' and t3 >= 37):
         print("E")
     else:
         print("N")
