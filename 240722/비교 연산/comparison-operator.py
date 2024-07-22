972beb1452380f6d33be871cc3f6bda874b2c9f9
minsol <163154019+soli-ziyo@users.noreply.github.com>
2024-07-22 15:09:33 +0900

    Title: 비교 연산, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240722/비교 연산/comparison-operator.py
@@ -0,0 +1,13 @@
+arr = input().split()
+a = int(arr[0])
+b = int(arr[1])
+
+if a>=b:
+    print(1)
+else:
+    0
+print(1) if a>b else print(0)
+print(1) if b>=a else print(0)
+print(1) if b>a else print(0)
+print(1) if a==b else print(0)
+print(1) if a!=b else print(0)
