dd80fe02fd9a0a0399e37ccc585ddf22ca4298db
minsol <163154019+soli-ziyo@users.noreply.github.com>
2024-07-22 15:13:13 +0900

    Title: 4가지 관계연산자, Time: 169ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240722/4가지 관계연산자/four-relational-operators.py
@@ -0,0 +1,9 @@
+arr = input().split()
+
+a = int(arr[0])
+b = int(arr[1])
+
+print(1) if a>=b else print(0)
+print(1) if a>b else print(0)
+print(1) if b>=a else print(0)
+print(1) if b>a else print(0)
