3a0dc0c697a5bbcfc03a8b2ce35d54404f5d088a
minsol <163154019+soli-ziyo@users.noreply.github.com>
2024-07-22 21:29:15 +0900

    Title: 중앙값 구하기, Time: 145ms, Memory: 24MB, Status: Passed - CodeTree

--- 240722/중앙값 구하기/find-the-median.py
+++ 240722/중앙값 구하기/find-the-median.py
@@ -4,9 +4,9 @@ a = int(a)
 b = int(b)
 c = int(c)
 
-if a<b and b<c:
+if (a<b and b<c) or (c<b and b<a):
     print(b)
-elif b<c and c<a:
+elif (b<c and c<a) or (a<c and c<b):
     print(c)
 else:
     print(a)
