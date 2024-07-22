d311f28a2f43bbca516ad2f4faf9ef52f19186dc
minsol <163154019+soli-ziyo@users.noreply.github.com>
2024-07-23 00:55:18 +0900

    Title: 청소당번 정하기, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240723/청소당번 정하기/cleaning-numbering.py
+++ 240723/청소당번 정하기/cleaning-numbering.py
@@ -2,13 +2,11 @@ n = int(input())
 cnt1= 0
 cnt2= 0
 cnt3= 0
-for i in range(n):
-    if i == 0:
-        continue 
-    if i%2==0 and i%3!=0 and i%12!=0:
-        cnt1 +=1
-    elif i %3 ==0 and i%12!=0:
-        cnt2 +=1
-    elif i%12 ==0:
+for i in range(1, n+1): #i의 숫자 몇부터 시작할지 지정할 수 있음 
+    if i %12 ==0:#가장 위에서부터 쓰는것이 우선순위 높은 것! 
         cnt3 +=1
+    elif i %3 ==0:
+        cnt2 +=1
+    elif i%2 ==0:
+        cnt1 +=1
 print(cnt1, cnt2, cnt3)
