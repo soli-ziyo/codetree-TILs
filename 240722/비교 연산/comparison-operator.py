ece4baa92602349673888b48650242e9e524685f
minsol <163154019+soli-ziyo@users.noreply.github.com>
2024-07-22 15:11:36 +0900

    Title: 비교 연산, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

--- 240722/비교 연산/comparison-operator.py
+++ 240722/비교 연산/comparison-operator.py
@@ -2,10 +2,7 @@ arr = input().split()
 a = int(arr[0])
 b = int(arr[1])
 
-if a>=b:
-    print(1)
-else:
-    0
+print(1) if a>=b else print(0)
 print(1) if a>b else print(0)
 print(1) if b>=a else print(0)
 print(1) if b>a else print(0)
