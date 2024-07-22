6111e63476c9c031c44d8e04c9f610b72b5862e6
minsol <163154019+soli-ziyo@users.noreply.github.com>
2024-07-22 21:13:44 +0900

    Title: 코로나 메뉴얼, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240722/코로나 메뉴얼/covid-manual.py
@@ -0,0 +1,13 @@
+cold1, temp1 = input().split()
+cold2, temp2= input().split()
+cold3, temp3 = input().split()
+
+temp1, temp2, temp3 = int(temp1), int(temp2), int(temp3)
+
+if (cold1 == "Y" and temp1 >=37):
+    if (cold2 == "Y" and temp2):
+        print("E")
+    elif (cold3 == "Y" and temp3):
+        print("E")
+    else:
+        print("N")
