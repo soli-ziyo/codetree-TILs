bdfd1355b9ad6f30e35e29f14d923f08d1f7637e
minsol <163154019+soli-ziyo@users.noreply.github.com>
2024-07-24 00:33:58 +0900

    Title: 조건에 따른 연산, Time: 100ms, Memory: 24MB, Status: Passed - CodeTree

--- 240724/조건에 따른 연산/operatino-by-rule.py
+++ 240724/조건에 따른 연산/operatino-by-rule.py
@@ -2,14 +2,17 @@ n = int(input())
 cnt =0
 
 while True:
+    
+
     if n%2==0:
         n = 3*n +1
         cnt +=1
-    else:
-        n = 2*n +1
+        if n >=1000:
+            break
+    elif n %2 !=0:
+        n = 2*n +2
         cnt +=1
-    
-    if n >=1000:
-        break
+        if n >=1000:
+            break
 
 print(cnt)
