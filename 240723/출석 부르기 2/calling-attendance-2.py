79b6f9fd7e14acf8aeeaa66151c758db8dcf7030
minsol <163154019+soli-ziyo@users.noreply.github.com>
2024-07-23 23:29:05 +0900

    Title: 출석 부르기 2, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240723/출석 부르기 2/calling-attendance-2.py
@@ -0,0 +1,13 @@
+while True:
+    name= int(input())
+    if name ==1:
+        print("John")
+    elif name ==2:
+        print("Tom")
+    elif name==3:
+        print("Paul")
+    elif name ==4:
+        print("Sam")
+    else:
+        print("Vacancy")
+        break
