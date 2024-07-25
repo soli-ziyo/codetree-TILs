d278b8825c6c5eef0cb26a5466ed3b69f289b8df
minsol <163154019+soli-ziyo@users.noreply.github.com>
2024-07-26 02:11:38 +0900

    Title: 완전수, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240726/완전수/perfect-number.py
@@ -0,0 +1,11 @@
+a, b = map(int, input().split())
+cnt =0
+sum =0
+
+for i in range(a, b+1):
+    for j in range(1, i):
+        if i%j==0:
+             sum +=j
+    if sum == i:
+        cnt +=1
+print(cnt)
