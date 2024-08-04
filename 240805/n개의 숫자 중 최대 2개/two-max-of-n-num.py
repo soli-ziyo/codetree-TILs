6a484f625f1f07cd7e5fc9d2ee3fb9928377c83a
minsol <163154019+soli-ziyo@users.noreply.github.com>
2024-08-05 01:57:47 +0900

    Title: n개의 숫자 중 최대 2개, Time: 132ms, Memory: 24MB, Status: Passed - CodeTree

--- 240805/n개의 숫자 중 최대 2개/two-max-of-n-num.py
+++ 240805/n개의 숫자 중 최대 2개/two-max-of-n-num.py
@@ -5,14 +5,20 @@ import sys
 max = -sys.maxsize
 max2 = -sys.maxsize
 arr2 =[]
-
+cnt = 0
 for i in arr:
     if i >max:
         max = i
-        
+
 for i in arr:
-    if i != max:
-        if i >max2:
+    if i ==max:
+        cnt +=1 
+
+for i in arr:
+    if cnt ==1:
+        if i != max and i >max2:
             max2 = i
+    else:
+        max2 = max
 
 print(max, max2)
