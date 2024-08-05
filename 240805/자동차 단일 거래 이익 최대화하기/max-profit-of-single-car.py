3d45ba019b9d5f7677ea9d6e3892c4d0d24e787f
minsol <163154019+soli-ziyo@users.noreply.github.com>
2024-08-05 14:45:09 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

--- 240805/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
+++ 240805/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
@@ -1,21 +1,21 @@
+# 변수 선언 및 입력:
 n = int(input())
+price = list(map(int, input().split()))
 
-arr = list(map(int, input().split()))
-
-import sys
-min_val = sys. maxsize
-index = 0
-
+# 배열을 앞에서부터 순회하며 최소값을 갱신해줍니다.
+# 각 원소에 대하여 해당 시점의 최소값과의 차이가
+# 최대가 될 때를 갱신해줍니다.
+max_profit = 0
+min_price = price[0]
 for i in range(n):
-    if arr[i] < min_val:
-        min_val = arr[i] 
-        idx= i
-max_val = min_val
+    profit = price[i] - min_price
 
-if idx == n-1:
-    print(0)
-else:
-    for j in range(idx, n):
-        if arr[j] > max_val:
-            max_val = arr[j]
-    print(max_val-min_val)
+    # 답을 갱신해줍니다.
+    if profit > max_profit:
+        max_profit = profit
+
+    # 지금까지의 최솟값을 갱신해줍니다.
+    if min_price > price[i]:
+        min_price = price[i]
+    
+print(max_profit)
