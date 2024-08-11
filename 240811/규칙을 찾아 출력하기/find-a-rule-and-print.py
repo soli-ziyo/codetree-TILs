654c6f2ca5dd47e02759948740225109a65da2f3
minsol <163154019+soli-ziyo@users.noreply.github.com>
2024-08-11 16:04:26 +0900

    Title: 규칙을 찾아 출력하기, Time: 143ms, Memory: 24MB, Status: Passed - CodeTree

--- 240811/규칙을 찾아 출력하기/find-a-rule-and-print.py
+++ 240811/규칙을 찾아 출력하기/find-a-rule-and-print.py
@@ -1,20 +1,12 @@
-def print_pattern(n):
-    if n == 1:
-        print('*')
-    elif n == 2:
-        print('* *')
-        print('* *')
-    else:
-        # 첫 번째 줄: n개의 별을 출력
-        print('* ' * n)
+n=int(input())
 
-        # 중간 줄: 패턴을 맞춰 별과 공백 출력
-        for i in range(1, n-1):
-            print('* ' * (i+1) + '  ' * (n-i-2) + '*')
-
-        # 마지막 줄: n개의 별을 출력
-        print('* ' * n)
-
-# 예시 실행
-n = int(input())
-print_pattern(n)
+for i in range(n):
+    for j in range(n):
+        if n<=1:
+            print('*', end=' ')
+        else:
+            if (i and j)!=0 and (i and j)!=n-1 and i<=j:
+                print(' ', end=' ')
+            else:
+                print('*', end=' ')
+    print()
