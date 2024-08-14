c219fb4d9e28a7dacb30bb969a009014fafbf823
minsol <163154019+soli-ziyo@users.noreply.github.com>
2024-08-14 23:07:31 +0900

    Title: 최소공배수 구하기, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240814/최소공배수 구하기/find-the-least-common-multiple.py
+++ 240814/최소공배수 구하기/find-the-least-common-multiple.py
@@ -1,6 +1,6 @@
 arr = list(map(int, input().split()))
 
-for i in range(2, min(arr)+1):
+for i in range(1, min(arr)+1):
     if (max(arr) * i) % min(arr) == 0:
         print(max(arr)*i)
         break
